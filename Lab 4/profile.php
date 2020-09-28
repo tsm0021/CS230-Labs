4a6c0f394d3557fb58ae183a62bef9f804e28aa3
Trey <tsm0021@mix.wvu.edu>
2020-09-27 22:45:44 -0400

    Add files via upload

---
+++ Lab 4/profile.php
@@ -0,0 +1,16 @@
+<?php 
+require 'includes/header.php'
+?>
+
+<main>
+        <?php
+        if (isset($_SESSION['uid'])){
+            include 'html/profile.html';
+        }
+        else{
+            header("Location: ../login.php");
+            exit();
+        }
+        ?>
+      
+</main>
